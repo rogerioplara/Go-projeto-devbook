a47bbb68ffa5f2bf77dd192f60ded339130b08af
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-09-26 18:01:21 -0300

    controller de publicação

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -1,10 +1,48 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/autenticacao"
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 // CriarPublicacao adiciona uma nova publicação no banco de dados
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	// Pegar usuarioId do token
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	// Leitura do body da requisição
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	// Armazena o que foi lido no corpo da requisição na struct publicacao
+	var publicacao models.Publicacao
+	if erro = json.Unmarshal(corpoRequisicao, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	// Conexão com o banco de dados
+	db, erro := database.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
 }
 
 // BuscarPublicacoes busca as publicações que apareceriam no feed do usuário
---
+++ api/src/repositorios/publicacoes.go
@@ -0,0 +1,4 @@
+package repositorios
+
+type Publicacoes struct {
+}
